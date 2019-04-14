75f47b2abe297ce4e7c110385cd9f04024fc07ac
Emilija <e.petraskaite@student.vu.nl>
2019-04-14 22:37:55 +0200

    adding all files

---
+++ kernel/kernel.c
@@ -0,0 +1,15 @@
+#define VIDEO_START 0xb8000
+#define VGA_LIGHT_GRAY 7
+
+static void PrintString(char *str){
+    unsigned char *video = ((unsigned char *) VIDEO_START);
+    while(*str != '\0'){
+        *(video++) = *str++;
+        *(video++) = VGA_LIGHT_GRAY;
+    }
+}
+
+void Kernel_Main(void){
+    PrintString("Hello world!");
+    while(1);
+}
