edca3c1910d7ea628438eab31f6d5f17a6eff7f9
Emilija <e.petraskaite@student.vu.nl>
2019-04-25 22:55:11 +0200

    Somewhat working ISRs - 2

--- kernel/isrs.d
+++
@@ -1,2 +0,0 @@
-isrs.o: isrs.c sys.h idt.h types.h isrs.h kernel.h arch/x86_64/kernel.h \
- arch/x86_64/control_register.h mem.h scrn.h sizes.h
--- kernel/kernel.d
+++
@@ -1,2 +0,0 @@
-kernel.o: kernel.c sys.h idt.h types.h isrs.h kernel.h \
- arch/x86_64/kernel.h arch/x86_64/control_register.h mem.h scrn.h sizes.h
