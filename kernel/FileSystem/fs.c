3bcb259a22297c12fd911a8e14d7d936e045bea4
emilijapet <emilijapet@users.noreply.github.com>
2019-06-29 16:24:22 +0200

    Updated a couple functions, need to compile

--- kernel/FileSystem/fs.c
+++ kernel/FileSystem/fs.c
@@ -1,10 +1,8 @@
 #include "fs.h"
 #include "../types.h"
 
-super_block_t sb;
-block__t inode_block;
-
 int64_t first_free_inode(){
+    block__t cur_iblock = first_iblock;
     inode__t inode;
     uint64_t i, inode_block_offset = 0;
 
@@ -12,11 +10,11 @@ int64_t first_free_inode(){
         // move on to the next inode block and reset the index
         if(i == INODES_PER_BLOCK){
             i = 0;
-            &inode_block += sizeof(block__t);
+            &cur_iblock += sizeof(block__t);
             inode_block_offset += 1;
         }
 
-        inode = inode_block.inodes[i];
+        inode = cur_iblock.inodes[i];
         
         if(!inode.valid){
             return i + inode_block_offset * INODES_PER_BLOCK;
@@ -27,9 +25,10 @@ int64_t first_free_inode(){
 
 inode_t find_inode(uint64_t inumber){
     uint64_t inode_block_offset = (int) (inumber / INODES_PER_BLOCK);
-    &inode_block += inode_block_offset * sizeof(block__t);
+    block__t cur_iblock = first_iblock;
+    &cur_iblock += inode_block_offset * sizeof(block__t);
 
-    inode__t inode = inode_block.inodes[i - inode_block_offset * INODES_PER_BLOCK];
+    inode__t inode = cur_iblock.inodes[inumber - inode_block_offset * INODES_PER_BLOCK];
 
     if(inode.valid){
         return inode;
@@ -49,6 +48,7 @@ int64_t create_file(uint64_t size){
     if(inumber && block_address){
         inode__t inode = find_inode(inumber);
         inode.valid = 1;
+        inode.size = size;
 
         for(int i = 0; i < size / BLOCK_SIZE; i++){
             if(i >= POINTERS_PER_NODE){
@@ -67,6 +67,10 @@ int64_t create_file(uint64_t size){
 int64_t write_file(uint64_t inumber, char *data, uint64_t size){
     inode_t inode = find_inode(inumber);
     int i;
+
+    if(size > inode.size){
+        return -1;
+    }
 
     for(i = 0; i < sizeof(data) / BLOCK_SIZE; i++){
         if(i < POINTERS_PER_NODE){
@@ -129,5 +133,8 @@ int64_t remove_file(uint64_t inumber){
 }
 
 char* read_file(uint64_t inumber){
+    inode__t inode = find_inode(inumber);
     return 0;
 }
+
+int main(){}
--- kernel/FileSystem/fs.h
+++ kernel/FileSystem/fs.h
@@ -23,10 +23,29 @@ typedef struct inode {
 } inode_t;
 
 typedef struct block {
-    super_block_t super;       // the data of a superblock
     inode_t inodes[INODES_PER_BLOCK]; // the data of an inode block
     uint64_t pointers[POINTERS_PER_BLOCK];  // the data of an indirect pointer block
     char data[BLOCK_SIZE];  // the data of a data block
 } block_t;
+
+// save the super block and the first inode block here. The super block should
+// be the very first entry in the disk, followed by any other reserved data, followed by the
+// inode blocks.
+super_block_t super;
+block__t first_iblock;
+
+void initialise_fs(uint64_t memory_size, uint64_t reserved_memory_size){
+    &super = 0x0;
+    super.magic_number = 0xf0f03410;
+
+    // 90% of the total available memory will be the number of data blocks, 10% will be the inode blocks
+    uint64_t available_memory_blocks = (memory_size - reserved_memory_size - sizeof(super_block_t)) / BLOCK_SIZE;
+    super.num_blocks = available_memory_blocks * 0.9;
+    super.num_iblocks = available_memory_blocks * 0.1;
+    super.num_inodes = super.super.num_iblocks * POINTERS_PER_NODE;
+
+    // set the location for the first iblock
+    &first_iblock = sizeof(super_block_t) + reserved_memory_size;
+}
 
 #endif
